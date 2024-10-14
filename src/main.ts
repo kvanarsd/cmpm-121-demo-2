89f3334bab95beabc771cee651e269cb8638d751
kvanarsd <kvanarsd@ucsc.edu>
2024-10-14 15:19:44 -0700

    Add rounded canvas

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,18 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const title = document.createElement("h1");
+title.innerHTML = "Demo 2";
+app.append(title);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+if(ctx) {
+    ctx.canvas.width = 256;
+    ctx.canvas.height = 256;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0, 256, 256);
+}
+
+app.append(canvas);
