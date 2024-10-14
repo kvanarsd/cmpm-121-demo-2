eb47414c43e0b7fea472b99af88d0066d120e856
kvanarsd <kvanarsd@ucsc.edu>
2024-10-14 15:21:18 -0700

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
 
-const title = document.createElement("h1");
-title.innerHTML = "Demo 2";
-app.append(title);
+const header = document.createElement("h1");
+header.innerHTML = "Demo 2";
+app.append(header);
 
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
@@ -18,5 +18,4 @@ if(ctx) {
     ctx.fillStyle = "white";
     ctx.fillRect(0,0, 256, 256);
 }
-
 app.append(canvas);
